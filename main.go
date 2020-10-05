8edf70ec7a3e4dcc95f690f89f57ae210aa0813e
Issac Trotts <issac.trotts@gmail.com>
2020-10-05 14:38:49 -0700

    add context to errors

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
-var ep = flag.String("ep", "", "URL of GraphQL endpoint")
-var q = flag.String("q", "", "GraphQL query to run")
-
 func main() {
+	ep := flag.String("ep", "", "URL of GraphQL endpoint")
+	q := flag.String("q", "", "GraphQL query to run")
 	flag.Parse()
 	if *ep == "" {
 		log.Fatal("missing required -ep flag for endpoint")
@@ -20,12 +21,22 @@ func main() {
 	if *q == "" {
 		log.Fatal("missing required -q flag for GraphQL query")
 	}
-	resp, err := http.Post(*ep, "application/graphql", strings.NewReader(*q))
-	if err != nil {
+	if err := gq(*q, *ep); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func gq(q, ep string) error {
+	resp, err := http.Post(ep, "application/graphql", strings.NewReader(q))
+	if err != nil {
+		return errors.Wrap(err, "POSTing GraphQL query")
 	}
 	defer resp.Body.Close()
-	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-		log.Fatal(err)
+	if err != nil {
+		return errors.Wrap(err, "reading response from GraphQL server")
 	}
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		return errors.Wrap(err, "outputting response")
+	}
+	return nil
 }
